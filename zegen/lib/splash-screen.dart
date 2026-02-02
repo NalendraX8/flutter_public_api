ff0512498abd477575d66ab2c1ee4554040ad758
NalendraX8 <nalendra.c.m@gmail.com>
2026-02-03 05:55:12 +0700

    First push

---
+++ zegen/lib/splash-screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'login.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+ _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+}
+@override
+
+Widget build(BuildContext context) {
+
+  }
+}
