837d33f11950b3a3e362e014e0d9b72483ada815
NalendraX8 <nalendra.c.m@gmail.com>
2026-02-03 10:43:50 +0700

    completed

--- zegen/lib/main.dart
+++ zegen/lib/main.dart
@@ -1,1 +1,34 @@
-import '';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'login.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+ _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+}
+@override
+Widget build(BuildContext context) {
+  return Scaffold(backgroundColor: Colors.blueAccent, body: Center(
+    child: Column(mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Icon(Icon.flash_on, size: 80, color: Colors.white),
+      const
+
+    ],),) );
+
+  }
+}
